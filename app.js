eb5e5033bca85c10e172d761e8dceca895f0e73c
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-20 11:42:12 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/* 
+Predisponiamo due input, in cui inseriremo Nome e Cognome.
+In un paragrafo utilizzare Nome e Cognome per salutare l’utente
+*/
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        firstname:'',
+        lastname:'',
+    }
+
+})
